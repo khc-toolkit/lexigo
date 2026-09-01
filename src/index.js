73435be89c6163acd43f24dac8568990340db580
khe4oyan <khe4oyan@gmail.com>
2026-09-01 23:52:23 +0400

    [feat] githug pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // libs
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 // import { Provider } from "react-redux";
 
 // store
@@ -11,9 +11,9 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     {/* <Provider store={store}> */}
     <App />
     {/* </Provider> */}
-  </BrowserRouter>,
+  </HashRouter>,
 );
