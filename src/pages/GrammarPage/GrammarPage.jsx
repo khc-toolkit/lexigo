66694ff71c080b038330e561e43f863cd52ca048
khe4oyan <khe4oyan@gmail.com>
2026-09-05 20:53:37 +0400

    feat: grammar page implemented

--- src/pages/GrammarPage/GrammarPage.jsx
+++ src/pages/GrammarPage/GrammarPage.jsx
@@ -11,21 +11,70 @@ import GRAMMAR from "../../constants/GRAMMAR";
 import classes from "./styles.module.css";
 
 export default function GrammarPage() {
-  const [activeTenseInd, setActiveTenseInd] = useState(0);
-
+  const [activeTenseInd, setActiveTenseInd] = useState(-1);
   const activeTense = GRAMMAR[activeTenseInd];
+
+  const prevTab = () => {
+    setActiveTenseInd((prev) => prev - 1);
+  };
+
+  const nextTab = () => {
+    setActiveTenseInd((prev) => prev + 1);
+  };
+
+  const isDisabledPrev = activeTenseInd - 1 === 0;
+  const isDisabledNext = activeTenseInd + 1 === GRAMMAR.length;
 
   return (
     <div className={classes.root}>
-      <div>
-        <h2>{activeTense.title}</h2>
-      </div>
+      {activeTenseInd > -1 ? (
+        <>
+          <button
+            className={classes.backButton}
+            onClick={() => setActiveTenseInd(-1)}
+          >
+            Վերադառնալ
+          </button>
+          <div>
+            <h2>{activeTense.title}</h2>
+          </div>
 
-      <div className={classes.items}>
-        {activeTense.data.map((tenseData, ind) => (
-          <TenseItem data={tenseData} key={ind} />
-        ))}
-      </div>
+          <div className={classes.items}>
+            {activeTense.data.map((tenseData, ind) => (
+              <TenseItem data={tenseData} key={ind} />
+            ))}
+          </div>
+
+          <div className={classes.navigationButtons}>
+            <button
+              className={classes.navigationButton}
+              disabled={isDisabledPrev}
+              onClick={prevTab}
+            >
+              {"<-"} Prev
+            </button>
+            <button
+              className={classes.navigationButton}
+              disabled={isDisabledNext}
+              onClick={nextTab}
+            >
+              Next {"->"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className={classes.grammars}>
+          {GRAMMAR.map((gramData, ind) => (
+            <button
+              onClick={() => setActiveTenseInd(ind)}
+              className={classes.grammarButton}
+              key={ind}
+            >
+              {gramData.title}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
